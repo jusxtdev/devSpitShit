7a18e334b22aca1ab650fdcecfec2acc37f2828d
Dev Prajapati <pdev25214@gmail.com>
2026-06-05 19:17:49 +0000

    Add prisma client

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,3 +1,12 @@
+import "dotenv/config";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client.js";
+import { env } from "../env.js";
 
-PrismaClient
+
+const connectionString = env.DATABASE_URL;
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
