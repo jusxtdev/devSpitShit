14c640026e20a8f869abe3c13e2a9ca758746d88
Dev Prajapati <pdev25214@gmail.com>
2026-06-05 19:33:09 +0000

    Add passwords

---
+++ server/README.md
@@ -0,0 +1,8 @@
+# Backend for my personal blogging platform
+
+## Differnet operations supported on each entity
+Entities - Blog, Comment, User              
+
+### Auth
+- Only username + password auth + JWT (header + cookie)
+- 
--- server/src/schema/user.schema.ts
+++ server/src/schema/user.schema.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 const createUser = z.object({
-    username : z.string().min(3).max(100)
+    username : z.string().min(3).max(100),
+    password : z.string().min(3).max(20)
 })
 export type createUserInput = z.infer<typeof createUser>
 
